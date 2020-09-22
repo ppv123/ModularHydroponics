2774feceba9990fbb068e9eb86d3d8a8a2f6dad9
ppv123 <66459755+ppv123@users.noreply.github.com>
2020-09-22 15:42:54 +0900

    Update sensing.py

--- sensing.py
+++ sensing.py
@@ -5,15 +5,16 @@ import adafruit_am2320
 import smbus
 
 I2C_CH_1 = 1
-
+CONT_H_RES_MODE = 0x10
 BH1750_DEV_ADDR = 0x23
 
-CONT_H_RES_MODE = 0x10
+'''
 CONT_H_RES_MODE2 = 0x11
 CONT_L_RES_MODE = 0x13
 ONETIME_H_RES_MODE = 0x20
 ONETIME_H_RES_MODE2 = 0x21
 ONETIME_L_RES_MODE = 0x23
+'''
 
 i2c = smbus.SMBus(I2C_CH)   # pcf8574 io expansion board 제품 확인
 
@@ -22,6 +23,7 @@ am = adafruit_am2320.AM2320(i2c)
 
 
 def getLux():
+	luxBytes = i2c2.read_i2c_block_data(BH1750_DEV_ADDR, CONT_H_RES_MODE,2)
 	lux = int.from_bytes(luxBytes, byteorder=’big’)
 	return lux
 
