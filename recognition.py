6742613d28eda1f1d4916e994cf67edcdb0b509f
saengGarlic <jwh6290@gmail.com>
2020-10-22 01:42:18 +0900

    directory clean

--- recognition.py
+++ recognition.py
@@ -1,19 +1,24 @@
-from ModularHydroponics import i2cinit
+import smbus
 
 
 def check():
     checking = {}
 
-    i2cinit()
+    bus_number = 1
+    bus = smbus.SMBus(bus_number)
 
     for i in range(0, 2):
         for device in range(128):
             try:
                 bus.write_byte(device, 0)
                 if hex(device) == "0x23":
+                    checking['lux'] = 0x23
 
-                if:
-                    pass
+                if hex(device) == "0x5c":
+                    checking['tempHum'] = 0x5c
+
+                if hex(device) == "0x38":
+                    checking['ph'] = 0x38
 
             except():
                 pass
