4c7b6c41c1f9728f94e63b563c78de1f27d86969
ppv123 <66459755+ppv123@users.noreply.github.com>
2020-11-07 18:47:35 +0900

    Update menu.py

--- test/menu.py
+++ test/menu.py
@@ -1,5 +1,7 @@
 import sys
 import traceback
+import LCD_driver
+import time
 
 import os
 
@@ -9,8 +11,20 @@ class Menu(object):
         self.title = title
         self.menuIndex = index
         self.prevMenu = self
-        self.subMenuObj = []    #하위메뉴 목록 리스트들(딕셔너리 타입)
-        #self.disp1602mode = False
+        self.subMenuObj = []  # 하위메뉴 목록 리스트들(딕셔너리 타입)
+        self.display = LCD_driver.lcd()  # lcd 클래스의 인스턴스: display
+
+    def lcd_write(self, display, text='', num_line=1, num_cols=15):
+        if (len(text) > num_cols):
+            display.lcd_display_string(text[:num_cols], num_line)
+            time.sleep(1)
+            for i in range(len(text) - num_cols + 1):
+                text_to_print = text[i:i + num_cols]
+                display.lcd_display_string(text_to_print, num_line)
+                time.sleep(0.2)
+            time.sleep(1)
+        else:
+            display.lcd_display_string(text, num_line)
 
     def run(self):
         self.menu_listing()
@@ -18,26 +32,34 @@ class Menu(object):
     def is_top(self):
         return self.title == self.prevMenu.title and self.menuIndex == self.prevMenu.menuIndex
 
-    def menu_listing(self):
+    def menu_listing(self):  # 메뉴들을 화면에 표시
         print('\n')
         for i in range(len(self.title) + 48):
             print("=", end='')
         print("\n", self.title)
+        self.lcd_write(self.display, self.title, 1)
         for i in range(len(self.title) + 48):
             print("=", end='')
         print("")
         for i in range(self.subMenuObj.__len__()):
             print(self.subMenuObj[i].menuIndex, ' - ', self.subMenuObj[i].title, end='')
-            if isinstance(self.subMenuObj[i], ToggleOption):    #toggleoption 활성화
+            if isinstance(self.subMenuObj[i], ToggleOption):  # toggleoption 활성화
                 print('  >>  Status / ', self.subMenuObj[i].getstatus())
             else:
                 print('')
-        #아래는 '0 - Back' 출력
+        # 아래는 '0 - Back' 출력
         for i in range(len(self.title) + 48):
             print("=", end='')
         print("\n0 - Back")
         for i in range(len(self.title) + 48):
             print("=", end='')
+
+        while True:
+            for i in range(self.subMenuObj.__len__()):
+                self.lcd_write(self.display, 'go = ' + self.subMenuObj[i].menuIndex + '-' + self.subMenuObj[i].title, 2)
+                time.sleep(1)
+            self.lcd_write(self.display, 'go = ' + '0-Back', 2)
+
 
 
 '''
@@ -47,6 +69,8 @@ class Menu(object):
             #표시 못하고 넘어가는 내용은 임시저장
             #키보드조작(다음으로 넘어갈때) 나머지 메뉴 표시
 '''
+
+
 class SubMenu(Menu):
     def __init__(self, title, index, prevmenu):
         super().__init__(title, index)
@@ -69,6 +93,7 @@ class ExecOption(SubMenu):
         try:
             self.opt_guide('This is executable option for test')
             self.function(**self.funcargs)
+            self.lcd_write(self.display, 'ExecOp', 2)
 
         except:
             sys.stderr.write('executable option execution failed')
