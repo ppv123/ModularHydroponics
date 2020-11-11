846aacf0bbcefeefae9f79881cc3904ba8a4ff06
ppv123 <66459755+ppv123@users.noreply.github.com>
2020-11-12 00:24:02 +0900

    Create i2c_lib.py

---
+++ ModularHydroponics/i2c_lib.py
@@ -0,0 +1,35 @@
+import smbus
+import time
+
+
+class i2c_device:
+    def __init__(self, addr, port=1):
+        self.addr = addr
+        self.bus = smbus.SMBus(port)
+
+    # Write a single command
+    def write_cmd(self, cmd):
+        self.bus.write_byte(self.addr, cmd)
+        time.sleep(0.0001)
+
+    # Write a command and argument
+    def write_cmd_arg(self, cmd, data):
+        self.bus.write_byte_data(self.addr, cmd, data)
+        time.sleep(0.0001)
+
+    # Write a block of data
+    def write_block_data(self, cmd, data):
+        self.bus.write_block_data(self.addr, cmd, data)
+        time.sleep(0.0001)
+
+    # Read a single byte
+    def read(self):
+        return self.bus.read_byte(self.addr)
+
+    # Read
+    def read_data(self, cmd):
+        return self.bus.read_byte_data(self.addr, cmd)
+
+    # Read a block of data
+    def read_block_data(self, cmd):
+        return self.bus.read_block_data(self.addr, cmd)
