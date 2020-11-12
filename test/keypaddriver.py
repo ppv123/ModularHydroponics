469bbac4b2b70cb840bddaa57e4424af16a2aa1b
ppv123 <66459755+ppv123@users.noreply.github.com>
2020-11-12 17:10:48 +0900

    Update keypaddriver.py

--- test/keypaddriver.py
+++ test/keypaddriver.py
@@ -1,9 +1,10 @@
 import RPi.GPIO as GPIO
+import time
+import sys
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 class keypad:
@@ -11,10 +12,33 @@ class keypad:
         while True:
             up_state = GPIO.input(18)
             select_state = GPIO.input(23)
-            down_state = GPIO.input(24)
             if not up_state:
-                return 1
+                time.sleep(0.2)
+                return True
             elif not select_state:
-                return 0
-            elif not down_state:
-                return -1
+                time.sleep(0.2)
+                return False
+
+    def key_count(self):
+        try:
+            count = 0
+            while True:
+                putB = button.key_input()
+                if putB:
+                    time.sleep(0.5)
+                    while True:
+                        putB = button.key_input()
+                        if putB:
+                            time.sleep(0.1)
+                            count += 1
+                            #display.lcd_long_write(display, str(count), 1)
+                        if not putB:
+                            time.sleep(0.1)
+                            #display.lcd_long_write(display, 'count: ' + str(count), 2)
+                            break
+                break
+
+        except Keyboardinterrupt:
+            #display.lcd_long_write(display, '-cleaning up- ', 1)
+            #display.lcd_clear()
+            sys.exit(1)
